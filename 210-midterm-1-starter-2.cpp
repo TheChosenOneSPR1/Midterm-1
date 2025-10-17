e18a4a7ea15d206b73ea2b6a482776d89cca9e93
Shivam Rijal <rijallprasadshivam@gmail.com>
2025-10-16 19:27:47 -0700

    comment

--- 210-midterm-1-starter-2.cpp
+++ 210-midterm-1-starter-2.cpp
@@ -1,67 +1,67 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // This is for the input/output count and cin
+using namespace std; // This is so we dont have to type std:: before every cout and cin in the code
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20; // This is the constant 
 
-class DoublyLinkedList {
-private:
-    struct Node {
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
-            data = val; 
-            prev = p;
-            next = n;
+class DoublyLinkedList { // This is the class for the DoublyLinkedList
+private: // This is so that only the class cna use these variables and functions
+    struct Node { // This is the struct for the Node which holds the data and the pointers.
+        int data; // Stores the value of the node
+        Node* prev; // This is the pointer to the previous node so its going backwards
+        Node* next; // This is the pointer to the next node so its going forwards
+        Node(int val, Node* p = nullptr, Node* n = nullptr) { // This is the Constructor for the Node that sets the data and the pointers
+            data = val; // stores the value of the node
+            prev = p; // stores the pointer to the previous node
+            next = n; // stores the pointer to the next node
         }
     };
 
-    Node* head;
-    Node* tail;
+    Node* head; // This is the pointer for the first node
+    Node* tail; // This is the pointer for the last node
 
-public:
-    DoublyLinkedList() { head = nullptr; tail = nullptr; }
+public: // This is so that the code under can be used anywhere
+    DoublyLinkedList() { head = nullptr; tail = nullptr; } // This is the Constructor that makes the empty list
 
-    void insert_after(int value, int position) {
-        if (position < 0) {
+    void insert_after(int value, int position) { // This adds a new node after a certain position
+        if (position < 0) { // makes sure that it is a postive and not a negative
             cout << "Position must be >= 0." << endl;
+            return; // The exit
+        }
+
+        Node* newNode = new Node(value); // This makes the new node
+        if (!head) { // If the list is empty
+            head = tail = newNode; //makes the new node the head and tail
             return;
         }
 
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode;
-            return;
-        }
-
-        Node* temp = head;
+        Node* temp = head; // this is the start for the head
         for (int i = 0; i < position && temp; ++i)
-            temp = temp->next;
+            temp = temp->next; //makes it go to the right postion
 
-        if (!temp) {
+        if (!temp) { // if the position is too far or large
             cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
+            delete newNode; // deletes the new node
             return;
         }
 
-        newNode->next = temp->next;
+        newNode->next = temp->next; // connects the new node to the middle of the list
         newNode->prev = temp;
         if (temp->next)
-            temp->next->prev = newNode;
+            temp->next->prev = newNode; // This connect the back link
         else
-            tail = newNode;
-        temp->next = newNode;
+            tail = newNode; // this updates the tail if it ends 
+        temp->next = newNode; // This links the current node to the new node
     }
 
-    void delete_val(int value) {
-        if (!head) return;
+    void delete_val(int value) { // deletes a node by the value
+        if (!head) return; // if there is nothing to delete it is empty
 
-        Node* temp = head;
+        Node* temp = head; // makes it start at the head
         
-        while (temp && temp->data != value)
-            temp = temp->next;
+        while (temp && temp->data != value) // goes through until you find the node that goes with the value
+            temp = temp->next; // goes to the next node
 
-        if (!temp) return; 
+        if (!temp) return; // if nothing is found it just returns
 
         if (temp->prev)
             temp->prev->next = temp->next;
